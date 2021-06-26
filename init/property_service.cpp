c414a2370cfc3c973a5a08f9c76563c665369b18
Ziyang Zhou <ziyang.zhou@outlook.com>
2021-06-26 05:42:24 +0000

    allow override ro.* prop

--- init/property_service.cpp
+++ init/property_service.cpp
@@ -1270,6 +1270,8 @@ static void ProcessKernelCmdline() {
     ImportKernelCmdline([&](const std::string& key, const std::string& value) {
         if (StartsWith(key, ANDROIDBOOT_PREFIX)) {
             InitPropertySet("ro.boot." + key.substr(ANDROIDBOOT_PREFIX.size()), value);
+        } else if (StartsWith(key, "ro.")) {
+            InitPropertySet(key, value);
         }
     });
 }
